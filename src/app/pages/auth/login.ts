eda967c10e8370716538af384a40ec92213b3d83
Jerick Signapan <jericksignapan3331@gmail.com>
2025-10-07 08:24:07 +0800

    refactor

--- src/app/pages/auth/login.ts
+++ src/app/pages/auth/login.ts
@@ -19,7 +19,7 @@ import { AppFloatingConfigurator } from '../../layout/component/app.floatingconf
                 <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
                     <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20" style="border-radius: 53px">
                         <div class="text-center mb-8">
-                            <img src="assets/icons/icon-512x512.png" alt="Logo" class="mb-8 w-16 h-16 mx-auto" />
+                            <img src="assets/icons/icon-48x48.png" alt="logo" class="mb-8 mx-auto" />
                             <div class="text-surface-900 dark:text-surface-0 text-3xl font-medium mb-4">Welcome to Lams!</div>
                             <span class="text-muted-color font-medium">Sign in to continue</span>
                         </div>
