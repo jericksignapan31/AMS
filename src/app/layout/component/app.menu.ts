946724fd2ce6394e68c5ac0b32805ca5dff23123
Jerick Signapan <jericksignapan3331@gmail.com>
2025-11-21 10:29:47 +0800

    add profile in sidebar

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -1,49 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { AppMenuitem } from './app.menuitem';
+import { UserService } from '../../pages/service/user.service';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
     selector: 'app-menu',
     standalone: true,
-    imports: [CommonModule, AppMenuitem, RouterModule],
+    imports: [CommonModule, AppMenuitem, RouterModule, AvatarModule],
+    styleUrls: ['../../../assets/layout/_menu.scss'],
     template: `<ul class="layout-menu">
+        <div class="user-panel" *ngIf="currentUser">
+            <div class="user-panel-avatar-container">
+                <img *ngIf="currentUser?.profilePicture" [src]="currentUser.profilePicture" width="64" class="user-panel-avatar" alt="avatar" />
+                <p-avatar *ngIf="!currentUser?.profilePicture" [label]="getInitials()" shape="circle" size="xlarge" styleClass="user-panel-avatar"></p-avatar>
+            </div>
+            <div class="user-panel-info">
+                <span>{{ currentUser?.firstName }} {{ currentUser?.lastName }}</span> <br />
+                <span class="user-panel-role">{{ currentUser?.role }}</span>
+            </div>
+        </div>
         <ng-container *ngFor="let item of model; let i = index">
             <li app-menuitem *ngIf="!item.separator" [item]="item" [index]="i" [root]="true"></li>
             <li *ngIf="item.separator" class="menu-separator"></li>
         </ng-container>
     </ul> `
 })
-export class AppMenu {
+export class AppMenu implements OnInit {
     model: MenuItem[] = [];
+    currentUser: any = null;
+
+    constructor(private userService: UserService) {}
 
     ngOnInit() {
+        this.loadUserProfile();
+        this.loadMenuItems();
+    }
+
+    loadUserProfile() {
+        this.userService.getUserProfile().subscribe({
+            next: (userData) => {
+                console.log('User profile loaded:', userData);
+                this.currentUser = userData;
+            },
+            error: (error) => {
+                console.error('Error loading user profile:', error);
+                // Fallback to localStorage if API fails
+                const storedUser = localStorage.getItem('currentUser');
+                if (storedUser) {
+                    this.currentUser = JSON.parse(storedUser);
+                }
+            }
+        });
+    }
+
+    getInitials(): string {
+        if (!this.currentUser) return 'U';
+        const firstName = this.currentUser.firstName || '';
+        const lastName = this.currentUser.lastName || '';
+        return (firstName.charAt(0) + lastName.charAt(0)).toUpperCase();
+    }
+
+    loadMenuItems() {
         this.model = [
             {
                 label: 'Home',
                 items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/app/dashboard'] }]
             },
-            // {
-            //     label: 'UI Components',
-            //     items: [
-            //         { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', routerLink: ['/uikit/formlayout'] },
-            //         { label: 'Input', icon: 'pi pi-fw pi-check-square', routerLink: ['/uikit/input'] },
-            //         { label: 'Button', icon: 'pi pi-fw pi-mobile', class: 'rotated-icon', routerLink: ['/uikit/button'] },
-            //         { label: 'Table', icon: 'pi pi-fw pi-table', routerLink: ['/uikit/table'] },
-            //         { label: 'List', icon: 'pi pi-fw pi-list', routerLink: ['/uikit/list'] },
-            //         { label: 'Tree', icon: 'pi pi-fw pi-share-alt', routerLink: ['/uikit/tree'] },
-            //         { label: 'Panel', icon: 'pi pi-fw pi-tablet', routerLink: ['/uikit/panel'] },
-            //         { label: 'Overlay', icon: 'pi pi-fw pi-clone', routerLink: ['/uikit/overlay'] },
-            //         { label: 'Media', icon: 'pi pi-fw pi-image', routerLink: ['/uikit/media'] },
-            //         { label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'] },
-            //         { label: 'Message', icon: 'pi pi-fw pi-comment', routerLink: ['/uikit/message'] },
-            //         { label: 'File', icon: 'pi pi-fw pi-file', routerLink: ['/uikit/file'] },
-            //         { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/uikit/charts'] },
-            //         { label: 'Timeline', icon: 'pi pi-fw pi-calendar', routerLink: ['/uikit/timeline'] },
-            //         { label: 'Misc', icon: 'pi pi-fw pi-circle', routerLink: ['/uikit/misc'] }
-            //     ]
-            // },
+
             {
                 label: 'Pages',
                 icon: 'pi pi-fw pi-briefcase',
@@ -70,26 +96,12 @@ export class AppMenu {
                         icon: 'pi pi-fw pi-pencil',
                         routerLink: ['/app/crud']
                     },
-                    // {
-                    //     label: 'Custodian',
-                    //     icon: 'pi pi-fw pi-users',
-                    //     routerLink: ['/app/custodian']
-                    // },
+
                     {
                         label: 'Request Maintenance',
                         icon: 'pi pi-fw pi-wrench',
                         routerLink: ['/app/requestmaintenance']
                     }
-                    // {
-                    //     label: 'Not Found',
-                    //     icon: 'pi pi-fw pi-exclamation-circle',
-                    //     routerLink: ['/pages/notfound']
-                    // },
-                    // {
-                    //     label: 'Empty',
-                    //     icon: 'pi pi-fw pi-circle-off',
-                    //     routerLink: ['/pages/empty']
-                    // }
                 ]
             },
             {
@@ -136,22 +148,6 @@ export class AppMenu {
                     }
                 ]
             }
-            // {
-            //     label: 'Get Started',
-            //     items: [
-            //         {
-            //             label: 'Documentation',
-            //             icon: 'pi pi-fw pi-book',
-            //             routerLink: ['/documentation']
-            //         },
-            //         {
-            //             label: 'View Source',
-            //             icon: 'pi pi-fw pi-github',
-            //             url: 'https://github.com/primefaces/sakai-ng',
-            //             target: '_blank'
-            //         }
-            //     ]
-            // }
         ];
     }
 }
